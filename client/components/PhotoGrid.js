17c355155465d6eb388da6960028ed31ae34adde
Victor Marks <vjmarks@gmail.com>
2019-04-24 15:22:57 -0700

    added a couple routes

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return <div className="photo-grid">I'm the photo grid</div>;
+  }
+});
+
+export default PhotoGrid;
