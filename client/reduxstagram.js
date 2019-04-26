74f62a08556d6d1926b64860266b429da98247c0
Victor Marks <vjmarks@gmail.com>
2019-04-25 17:06:06 -0700

    set up store, actions, and reducers

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,19 +4,23 @@ import { render } from 'react-dom';
 
 import css from './styles/style.styl';
 
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid} />
-      <Route path="/view/:postId" component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
 render(router, document.getElementById('root'));
