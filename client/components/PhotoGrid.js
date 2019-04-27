6c7f94f1d6638abc8fd6f5e9241b9acdb9bf0bf5
Victor Marks <vjmarks@gmail.com>
2019-04-26 23:58:51 -0700

    changed to class based components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Photo from '../Photo';
 
-const PhotoGrid = React.createClass({
+class PhotoGrid extends React.Component {
   render() {
-    return <div className="photo-grid">I'm the photo grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => {
+          return <Photo {...this.props} key={i} index={i} post={post} />;
+        })}
+      </div>
+    );
   }
-});
+}
 
 export default PhotoGrid;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Single = React.createClass({
+class Single extends React.Component {
   render() {
     return <div className="single-photo">I'm the single</div>;
   }
-});
+}
 
 export default Single;
